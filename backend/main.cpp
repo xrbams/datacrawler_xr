bf7ea90f618c22e5d0e81a9b37270e1f653201a0
xrbams <bmsakamali@gmail.com>
2025-02-23 12:27:20 +0200

    last save point

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,7 +1,8 @@
-#include "imports.h"
+#include "h/storage.hpp"
 
 int main(int argc, char const *argv[])
 {
     cout << "This is my first go at Concurrent Web Crawler" << endl;
+    getstorage();
     return 0;
 }
---
+++ backend/modules/src/storage.cpp
@@ -0,0 +1,5 @@
+#include "../h/storage.hpp"
+
+static void getstorage() {
+    cout << "Create the first instance for mongodb storage" << endl;
+}
