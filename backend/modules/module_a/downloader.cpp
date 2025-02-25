1661fe6fe04afee9f9ee6895db7717d76bca88b2
xrbams <bmsakamali@gmail.com>
2025-02-25 22:14:02 +0200

    Active basic web crawling

--- backend/modules/module_a/downloader.cpp
+++ backend/modules/module_a/downloader.cpp
@@ -12,13 +12,20 @@ Parsing: Gumbo, libxml2, or HTML Tidy.
 Multithreading: std::thread, std::async, or Boost.Thread.
 
 */
-
+// include libraries
 #include <iostream>
 #include <string>
 #include <chrono>
 #include <iomanip>
 #include <sstream>
-#include <curl/curl.h>  // Requires libcurl
+#include <memory>
+#include <curl/curl.h>
+#include <gumbo.h>
+
+// include built functions
+#include "metadata.cpp"
+
+using namespace std;
 
 /*
 - Fetching Content
@@ -31,10 +38,18 @@ private:
 std::string url = "";
 int status_code;
 std::string timestamp; // timestamp use time
+Metadata data_struct;
 
 // CURL callback function (not storing content yet)
-static size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp){
-    return size * nmemb; // Discard data for now
+static size_t writeCallback(void* ptr, size_t size, size_t nmemb, void* userdata){
+    size_t t_size = size * nmemb;
+    unique_ptr<string>* response = static_cast<std::unique_ptr<std::string>*>(userdata);
+    response->get()->append(static_cast<char*>(ptr), t_size); // store data
+
+    //std::cout << "Received Data Chunk: " 
+           // << std::string(static_cast<char*>(ptr), t_size) << std::endl;
+
+    return t_size; // Tell CURL how much we processed
 }
     
 public:
@@ -45,11 +60,16 @@ public:
     bool fetch() {
         CURL* curl = curl_easy_init();
 
-        if(!curl) std::cerr << "Failed to initialize CURL \n";
+        if(!curl) {
+            std::cerr << "Failed to initialize CURL \n"; 
+            return false;
+        }
+        auto res_data = make_unique<string>();
 
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // specify URL, change it to const char*
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects for SSL/TLS handshake
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback); // collect response
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res_data);
 
         CURLcode res = curl_easy_perform(curl);
         if (res != CURLE_OK)
@@ -58,6 +78,8 @@ public:
             curl_easy_cleanup(curl);
             return false;
         }
+
+        //std::cout << "\nFull Response:\n" << *res_data << std::endl;
 
         // Get HTTP status code
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status_code);
@@ -82,3 +104,8 @@ public:
 
 
 };
+
+
+Downloader::~Downloader()
+{
+}
